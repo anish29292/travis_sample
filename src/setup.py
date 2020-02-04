c8885bfdba90de35435e18c68bf8814d95089d0e
anish29292 <anish29292@gmail.com>
2020-02-04 11:39:53 +0100

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+install_requires = [
+    'pandas>=0.25.0',
+    'numpy>=1.15.4',
+    'functools']
+
+setup(name='PACKAGE NAME HERE',
+      version='0.0.1',
+      description='test',
+      author='YOUR NAME HERE',
+      install_requires=install_requires,
+      author_email='fake-email@gmail.com',
+      packages=['mypackage_one', 'mypackage_two'])
